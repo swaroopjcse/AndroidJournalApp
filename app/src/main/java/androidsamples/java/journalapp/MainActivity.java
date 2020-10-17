13829c6df02d9673f32bcfe7c4ebfc39b2aade4b
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2020-10-17 10:02:32 -0600

    Added: AddEntry activity, fab navigation

--- app/src/main/java/androidsamples/java/journalapp/MainActivity.java
+++ app/src/main/java/androidsamples/java/journalapp/MainActivity.java
@@ -2,13 +2,24 @@ package androidsamples.java.journalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int ADD_ENTRY_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchAddEntryActivity(View view) {
+        Intent intent = new Intent(this, AddEntry.class);
+        Log.d(TAG, "Launching Add Entry");
+        startActivityForResult(intent, ADD_ENTRY_REQUEST_CODE);
     }
 }
