770959e50e6e4de6d2ca19a2fd38f53130ad8ec8
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2020-10-19 01:40:28 -0600

    Clicking save on empty EditTexts now returns RESULT_CANCELED which is ignored

--- app/src/main/java/androidsamples/java/journalapp/AddEntryActivity.java
+++ app/src/main/java/androidsamples/java/journalapp/AddEntryActivity.java
@@ -22,11 +22,17 @@ public class AddEntryActivity extends AppCompatActivity {
     public void saveEntry(View view) {
         EditText editTitle = findViewById(R.id.edit_title);
         EditText editDuration = findViewById(R.id.edit_duration);
-        Log.d(TAG, "title: " + editTitle.getText().toString() + ", duration: " + editDuration.getText().toString());
-        Intent resultData = new Intent();
-        resultData.putExtra(KEY_TITLE, editTitle.getText().toString());
-        resultData.putExtra(KEY_DURATION, Integer.parseInt(editDuration.getText().toString()));
-        setResult(RESULT_OK, resultData);
+        String title = editTitle.getText().toString();
+        String duration = editDuration.getText().toString();
+        Log.d(TAG, "title: " + title + ", duration: " + duration);
+        if (title.isEmpty() || duration.isEmpty()) {
+            setResult(RESULT_CANCELED);
+        } else {
+            Intent resultData = new Intent();
+            resultData.putExtra(KEY_TITLE, title);
+            resultData.putExtra(KEY_DURATION, Integer.parseInt(duration));
+            setResult(RESULT_OK, resultData);
+        }
         finish();
     }
 
